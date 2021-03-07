004459f43adab317789949fed7855ada388f7de3
Sungjoon Park <sungjoon512@gmail.com>
2021-03-07 20:11:28 +0900

    switch between valid link and dummy link

--- src/components/pages/projects/stack.tsx
+++ src/components/pages/projects/stack.tsx
@@ -1,26 +1,47 @@
 import { FC } from 'react';
 import { IconExternalLink } from '@/components/atoms/icons';
 
+interface IStackTitleAndIconProps {
+  title: IStackProps['title'];
+  isValidLink: boolean;
+}
+
+const StackTitleAndIcon = ({ title, isValidLink }: IStackTitleAndIconProps) => (
+  <>
+    {title}
+    {isValidLink && (
+      <span className="inline-block align-middle w-4 h-4 mb-0.5 ml-1">
+        <IconExternalLink />
+      </span>
+    )}
+  </>
+);
+
 interface IStackProps {
   link: string;
   title: string;
 }
 
-export const Stack: FC<IStackProps> = ({ link, title, children }) => (
-  <div className="border-b border-dashed border-gray-900 dark:border-white mb-4 pt-4 pb-8">
-    <p className="font-bold dark:text-white">
-      <a
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="inline-block"
-      >
-        {title}
-        <span className="inline-block align-middle w-4 h-4 mb-0.5 ml-1">
-          <IconExternalLink />
-        </span>
-      </a>
-    </p>
-    <div className="text-sm">{children}</div>
-  </div>
-);
+export const Stack: FC<IStackProps> = ({ link, title, children }) => {
+  const isDummyLink = link === `#`;
+
+  return (
+    <div className="border-b border-dashed border-gray-900 dark:border-white mb-4 pt-4 pb-8">
+      <p className="font-bold dark:text-white">
+        {isDummyLink ? (
+          <StackTitleAndIcon title={title} isValidLink={!isDummyLink} />
+        ) : (
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block"
+          >
+            <StackTitleAndIcon title={title} isValidLink={!isDummyLink} />
+          </a>
+        )}
+      </p>
+      <div className="text-sm">{children}</div>
+    </div>
+  );
+};
