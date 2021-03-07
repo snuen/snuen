2519601db36a32487a29274d3ef1225e350b3f51
Sungjoon Park <sungjoon512@gmail.com>
2021-03-07 21:10:34 +0900

    edit skill to fit

--- src/components/molecules/skills.tsx
+++ src/components/molecules/skills.tsx
@@ -3,7 +3,7 @@ interface ISkillsProps {
 }
 
 export const Skills = ({ skillsText }: ISkillsProps) => (
-  <ul className="flex items-center">
+  <ul className="flex flex-wrap items-center -mb-2">
     {skillsText.split(`,`).map((skillText) => {
       const st = skillText.trim();
       const stl = st.toLowerCase();
@@ -11,7 +11,7 @@ export const Skills = ({ skillsText }: ISkillsProps) => (
       return (
         <li
           key={stl}
-          className={`h-6 px-2 mr-2 border rounded text-xs leading-6 text-${stl}`}
+          className={`h-6 px-2 mr-2 mb-2 border rounded text-xs leading-6 text-${stl}`}
         >
           {st}
         </li>
