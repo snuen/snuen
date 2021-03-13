3748ee199e6a8a143176f4ac79d24c95e90b908c
Sungjoon Park <sungjoon512@gmail.com>
2021-03-13 15:55:35 +0900

    Revert "add recoil for later use"
    This reverts commit b716be45bb2e05184fc66dab2d9f4dc38f491def.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,17 @@
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
-import { RecoilRoot } from 'recoil';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import '@/styles/globals.scss';
 import * as utils from '@/utils';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <RecoilRoot>
-    <QueryClientProvider client={utils.httpClient.queryClient}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </RecoilRoot>
+  <QueryClientProvider client={utils.httpClient.queryClient}>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 export default MyApp;
