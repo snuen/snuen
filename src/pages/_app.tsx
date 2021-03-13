b716be45bb2e05184fc66dab2d9f4dc38f491def
Sungjoon Park <sungjoon512@gmail.com>
2021-03-13 15:42:16 +0900

    add recoil for later use

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import '@/styles/globals.scss';
 import * as utils from '@/utils';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <QueryClientProvider client={utils.httpClient.queryClient}>
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <RecoilRoot>
+    <QueryClientProvider client={utils.httpClient.queryClient}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </RecoilRoot>
 );
 
 export default MyApp;
