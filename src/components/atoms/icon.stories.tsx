fb7597826c33b628d0ba5e72620db0ba5d43b20d
Sungjoon Park <sungjoon512@gmail.com>
2021-03-16 23:11:44 +0900

    update icon stories

--- src/components/atoms/icon.stories.tsx
+++ src/components/atoms/icon.stories.tsx
@@ -12,29 +12,35 @@ const Template: Story<IIconProps> = (args) => <Icon {...args} />;
 export const Me = Template.bind({});
 Me.args = {
   type: IconType.Me,
+  className: `inline-block w-6`,
 };
 
 export const Projects = Template.bind({});
 Projects.args = {
   type: IconType.Projects,
+  className: `inline-block w-6`,
 };
 
 export const Work = Template.bind({});
 Work.args = {
   type: IconType.Work,
+  className: `inline-block w-6`,
 };
 
 export const Moon = Template.bind({});
 Moon.args = {
   type: IconType.Moon,
+  className: `inline-block w-6`,
 };
 
 export const Sun = Template.bind({});
 Sun.args = {
   type: IconType.Sun,
+  className: `inline-block w-6`,
 };
 
 export const ExternalLink = Template.bind({});
 ExternalLink.args = {
   type: IconType.ExternalLink,
+  className: `inline-block w-6`,
 };
