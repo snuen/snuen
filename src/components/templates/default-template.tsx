70238f1bd1a187a55822f77ca9732f00ef8b3cb7
Sungjoon Park <sungjoon512@gmail.com>
2021-03-31 00:10:21 +0900

    add concept background

--- src/components/templates/default-template.tsx
+++ src/components/templates/default-template.tsx
@@ -9,11 +9,17 @@ interface IDefaultTemplate {
 export const DefaultTemplate = ({ isShowCard, children }: IDefaultTemplate) => (
   <div className="flex flex-col-reverse w-full min-h-screen md:flex-row">
     <Header />
-    <main className="flex-grow pt-4 pb-12 md:py-8 md:pr-8 md:pl-64">
-      <div className={isShowCard ? `container` : `container pt-4`}>
+    <main className="flex flex-grow pb-12 md:pb-0 md:pl-64">
+      <div
+        className={isShowCard ? `container pt-4 md:pt-8` : `container pt-12`}
+      >
         {isShowCard && <Card />}
         {children}
       </div>
+      <div
+        className="hidden h-full w-full bg-contain lg:block"
+        style={{ backgroundImage: `url(/concept-background.jpg)` }}
+      />
     </main>
   </div>
 );
