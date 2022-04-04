74a09cfa68bc2cb7cbb0c257ec6e686b116fb4f8
Sungjoon Park <sungjoon512@gmail.com>
2022-04-04 22:55:33 +0900

    I don't live there any more

--- src/contents/meta.ts
+++ src/contents/meta.ts
@@ -4,12 +4,12 @@ export const NAME = `Sungjoon Park`;
 
 export const SITE_LEAD = `Portfolio site of ${NAME} (${GITHUB_USERNAME})`;
 
-export const SITE_META = `This is a portfolio site of ${NAME}, a freelance frontend developer based in the city at the foot of Mt.Fuji`;
+export const SITE_META = `This is a portfolio site of ${NAME}, a freelance frontend developer based in the city of Kita, Tokyo.`;
 
 export const NAME_JA = `パク ソンジュン`;
 
 export const SITE_LEAD_JA = `${NAME_JA}のポートフォリオサイト（${GITHUB_USERNAME}）`;
 
-export const SITE_META_JA = `本サイトは、富士山の麓に住んでいるフリーランスフロントエンドエンジニア、${NAME_JA}のポートフォリオサイトです。`;
+export const SITE_META_JA = `本サイトは、東京都北区に住んでいるフリーランスフロントエンドエンジニア、${NAME_JA}のポートフォリオサイトです。`;
 
 export const MAIL_ADDRESS = `sungjoon512[atmark]gmail.com`;
