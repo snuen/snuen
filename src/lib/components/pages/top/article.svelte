6dca6b496c26595a1b29ab7cc46caed2e200678d
sjgently <sungjoon512@gmail.com>
2024-01-19 23:34:40 +0900

    just simply use json

--- src/lib/components/pages/top/article.svelte
+++ src/lib/components/pages/top/article.svelte
@@ -1,25 +1,15 @@
 <script lang="ts">
 	import Heading from '$lib/components/atoms/heading.svelte';
-
-	// @ts-expect-error import markdown file
-	import AboutMeJaContent from '$lib/contents/pages/top/about-me-ja.md';
-	// @ts-expect-error import markdown file
-	import TechPreferenceJaContent from '$lib/contents/pages/top/tech-preference-ja.md';
-	// @ts-expect-error import markdown file
-	import UsesJaContent from '$lib/contents/pages/top/uses-ja.md';
+	import contentJa from '$lib/contents/pages/top/content-ja.json';
 </script>
 
 <article>
-	<section class="mb-8">
-		<Heading level={2}>About me</Heading>
-		<AboutMeJaContent />
-	</section>
-	<section class="mb-8">
-		<Heading level={2}>Tech preference</Heading>
-		<TechPreferenceJaContent />
-	</section>
-	<section class="mb-8">
-		<Heading level={2}>Uses</Heading>
-		<UsesJaContent />
-	</section>
+	{#each contentJa.sections as section, i (i)}
+		<section class="mb-8">
+			<Heading level={2}>{section.title}</Heading>
+			{#each section.paragraphs as paragraph, j (j)}
+				<p>{paragraph}</p>
+			{/each}
+		</section>
+	{/each}
 </article>
