c827274a2ef7f9a22d38cdd3fca1daa9405e6920
sjgently <sungjoon512@gmail.com>
2024-01-21 11:45:32 +0900

    update projects

--- src/lib/components/pages/projects/article/index.svelte
+++ src/lib/components/pages/projects/article/index.svelte
@@ -5,21 +5,21 @@
 	import Project from './project.svelte';
 
 	const content = contentJa;
-	const clientContent = content.filter((item) => item.type === 'client');
 	const sideContent = content.filter((item) => item.type === 'side');
+	const clientContent = content.filter((item) => item.type === 'client');
 </script>
 
 <article>
 	<Heading level={2}>Projects</Heading>
 	<section class="mt-8 mb-8">
-		<Heading level={3}>Client projects</Heading>
-		{#each clientContent as { href, linkText, description, tags } (href)}
+		<Heading level={3}>Side projects</Heading>
+		{#each sideContent as { href, linkText, description, tags } (href)}
 			<Project {href} {linkText} {description} {tags} />
 		{/each}
 	</section>
-	<section class="mb-8">
-		<Heading level={3}>Side projects</Heading>
-		{#each sideContent as { href, linkText, description, tags } (href)}
+	<section class="mt-8 mb-8">
+		<Heading level={3}>Client projects</Heading>
+		{#each clientContent as { href, linkText, description, tags } (href)}
 			<Project {href} {linkText} {description} {tags} />
 		{/each}
 	</section>
