f650b4dd54ca66e3d737732b65c0a71bbabcaaae
sjgently <sungjoon512@gmail.com>
2024-01-27 22:12:16 +0900

    variable for later use

--- src/lib/components/pages/top/article.svelte
+++ src/lib/components/pages/top/article.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Heading from '$lib/components/atoms/heading.svelte';
 	import contentJa from '$lib/contents/pages/top/content-ja.json';
+
+	const content = contentJa;
 </script>
 
 <article>
-	{#each contentJa.sections as section, i (i)}
+	{#each content.sections as section, i (i)}
 		<section class="mb-8">
 			<Heading level={2}>{section.title}</Heading>
 			{#each section.paragraphs as paragraph, j (j)}
