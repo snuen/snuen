3779a2d1eca80e5fd39043a946117d7ac9636342
sjgently <sungjoon512@gmail.com>
2024-02-23 14:07:45 +0900

    use helper function and dynamic classes

--- src/lib/components/atoms/heading.svelte
+++ src/lib/components/atoms/heading.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
+	import { cn } from '$lib/utils/tw-helper';
+
 	export let level: 1 | 2 | 3;
+
+	const baseClassNames = ['font-semibold', 'my-[0.25em]'] as const;
 </script>
 
 {#if level === 1}
-	<h1 class="text-3xl font-semibold">
+	<h1 class={cn([...baseClassNames, 'text-1xl'])}>
 		<slot />
 	</h1>
 {:else if level === 2}
-	<h2 class="text-2xl font-semibold">
+	<h2 class={cn([...baseClassNames, 'text-2xl'])}>
 		<slot />
 	</h2>
 {:else if level === 3}
-	<h3 class="text-xl font-semibold">
+	<h3 class={cn([...baseClassNames, 'text-3xl'])}>
 		<slot />
 	</h3>
 {/if}
-
-<style>
-	h1,
-	h2,
-	h3 {
-		margin-top: 0.25em;
-		margin-bottom: 0.25em;
-	}
-</style>
