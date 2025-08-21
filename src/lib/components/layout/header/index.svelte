f2be552c486d7270b1c67b415b43b6beacb5c233
snuen <sungjoon512@gmail.com>
2025-08-21 21:59:40 +0900

    Refactor header component markup for improved readability

--- src/lib/components/layout/header/index.svelte
+++ src/lib/components/layout/header/index.svelte
@@ -7,9 +7,7 @@
   class="col-start-1 col-end-2 fixed right-0 bottom-0 left-0 md:static z-10 md:w-64 md:py-10 md:px-6 bg-base-200"
 >
   <nav class="md:sticky md:top-10 md:h-[calc(100vh-5rem)]">
-    <ul
-      class="menu menu-horizontal md:menu-vertical flex w-auto px-0 md:h-full"
-    >
+    <ul class="menu menu-horizontal md:menu-vertical w-auto px-0 md:h-full">
       <NavLinks />
       <li
         class="px-2 border-l border-dashed border-l-current md:border-none md:mb-2"
