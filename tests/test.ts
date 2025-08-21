6300ca88944324ec863b6bb93ebe719dfa1e8e86
snuen <sungjoon512@gmail.com>
2025-08-21 21:51:52 +0900

    Update heading selector in test for improved matching of whitespace

--- tests/test.ts
+++ tests/test.ts
@@ -3,6 +3,6 @@ import { expect, test } from '@playwright/test';
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
   await expect(
-    page.getByRole('heading', { name: 'パク ソンジュン' })
+    page.getByRole('heading', { name: /パク\s*ソンジュン/ })
   ).toBeVisible();
 });
