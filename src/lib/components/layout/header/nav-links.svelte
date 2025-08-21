a73795a890bb70ae0886b891f270f6810440235b
snuen <sungjoon512@gmail.com>
2025-08-21 21:43:08 +0900

    Refactor class attribute concatenation in nav-links component for improved readability

--- src/lib/components/layout/header/nav-links.svelte
+++ src/lib/components/layout/header/nav-links.svelte
@@ -21,9 +21,7 @@
 
 {#each navList as { label, href, Svg }, i ({ href })}
   <li
-    class={'grow md:grow-0 px-2' +
-      ' ' +
-      (i === navList.length - 1 ? 'md:mb-auto' : 'md:mb-2')}
+    class={`grow md:grow-0 px-2 ${i === navList.length - 1 ? 'md:mb-auto' : 'md:mb-2'}`}
   >
     <a
       {href}
